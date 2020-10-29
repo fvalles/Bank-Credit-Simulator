a51859a21da086dfc18d10058c2367e737820bc5
Federico Valles <fedeva_37@hotmail.com>
2020-10-29 13:16:48 -0300

    Simple state changed into Object state

--- src/components/CreditSimulator/index.js
+++ src/components/CreditSimulator/index.js
@@ -7,84 +7,96 @@ import './style.css'
 export default function CreditSimulator() {
   /* No olvidar:
   1) Formatear num con sep de miles
-  2) Ver colores azul bkg afinarlos
-  3) Dividir los componentes en carpetas distintas
   4) Ver como importo CSS para que no queden estilos globales / CSS Modules / Sass
   5) Refactorear funciones handlers y ver si conglomero states en 2 objetos de 3 key-values
   6) Aplicar alguna funcionalidad a los 2 botones. Una puede ser un alert sencillo y el otro un pop-up
   o una lista que se despliegue en la pantalla
   7) ordernar alfabeticamente styles clases */
-  const minCreditValue = 5000
-  const maxCreditValue = 50000
+  const minCredit = 5000
+  const maxCredit = 50000
   const minPeriod = 3
   const maxPeriod = 24
 
-  const [credit, setCredit] = useState(minCreditValue)
-  const [inputCredit, setInputCredit] = useState(minCreditValue)
-  const [sliderCredit, setSliderCredit] = useState(minCreditValue)
+  const [credit, setCredit] = useState({
+    value: minCredit,
+    inputValue: minCredit,
+    sliderValue: minCredit,
+  })
 
-  const [period, setPeriod] = useState(minPeriod)
-  const [inputPeriod, setInputPeriod] = useState(minPeriod)
-  const [sliderPeriod, setSliderPeriod] = useState(minPeriod)
+  const [period, setPeriod] = useState({
+    value: minPeriod,
+    inputValue: minPeriod,
+    sliderValue: minPeriod,
+  })
 
   const handleCreditInputChange = ({ target }) => {
     const inputValue = parseInt(target.value, 10)
-    setInputCredit(inputValue)
-    if (inputValue >= minCreditValue && inputValue <= maxCreditValue) {
-      setSliderCredit(inputValue)
-      setCredit(inputValue)
+    let validValue = false
+    if (inputValue >= minCredit && inputValue <= maxCredit) {
+      validValue = true
+    }
+    if (validValue) {
+      setCredit({ value: inputValue, inputValue, sliderValue: inputValue })
+    } else {
+      setCredit(prevCredit => {
+        return {
+          ...prevCredit,
+          inputValue,
+        }
+      })
     }
   }
 
   const handleCreditInputBlur = ({ target }) => {
-    const inputValue = parseInt(target.value, 10)
-    let validValue = null
-    if (inputValue < minCreditValue) {
-      validValue = minCreditValue
-    } else if (inputValue > maxCreditValue) {
-      validValue = maxCreditValue
+    const value = parseInt(target.value, 10)
+    let inputValue = null
+    if (value < minCredit) {
+      inputValue = minCredit
+    } else if (value > maxCredit) {
+      inputValue = maxCredit
     }
-    if (validValue) {
-      setInputCredit(validValue)
-      setSliderCredit(validValue)
-      setCredit(validValue)
+    if (inputValue) {
+      setCredit({ value: inputValue, inputValue, sliderValue: inputValue })
     }
   }
 
   const handleCreditSliderChange = sliderValue => {
-    setSliderCredit(sliderValue)
-    setInputCredit(sliderValue)
-    setCredit(sliderValue)
+    setCredit({ value: sliderValue, inputValue: sliderValue, sliderValue })
   }
 
   const handlePeriodInputChange = ({ target }) => {
     const inputValue = parseInt(target.value, 10)
-    setInputPeriod(inputValue)
+    let validValue = false
     if (inputValue >= minPeriod && inputValue <= maxPeriod) {
-      setSliderPeriod(inputValue)
-      setPeriod(inputValue)
+      validValue = true
+    }
+    if (validValue) {
+      setPeriod({ value: inputValue, inputValue, sliderValue: inputValue })
+    } else {
+      setPeriod(prevPeriod => {
+        return {
+          ...prevPeriod,
+          inputValue,
+        }
+      })
     }
   }
 
   const handlePeriodInputBlur = ({ target }) => {
-    const inputValue = parseInt(target.value, 10)
-    let validValue = null
-    if (inputValue < minPeriod) {
-      validValue = minPeriod
-    } else if (inputValue > maxPeriod) {
-      validValue = maxPeriod
+    const value = parseInt(target.value, 10)
+    let inputValue = null
+    if (value < minPeriod) {
+      inputValue = minPeriod
+    } else if (value > maxPeriod) {
+      inputValue = maxPeriod
     }
-    if (validValue) {
-      setInputPeriod(validValue)
-      setSliderPeriod(validValue)
-      setPeriod(validValue)
+    if (inputValue) {
+      setPeriod({ value: inputValue, inputValue, sliderValue: inputValue })
     }
   }
 
   const handlePeriodSliderChange = sliderValue => {
-    setSliderPeriod(sliderValue)
-    setInputPeriod(sliderValue)
-    setPeriod(sliderValue)
+    setPeriod({ value: sliderValue, inputValue: sliderValue, sliderValue })
   }
 
   return (
@@ -92,29 +104,29 @@ export default function CreditSimulator() {
       <h1>Simulá tu crédito</h1>
       <Selector
         currency="$"
-        inputValue={inputCredit}
+        inputValue={credit.inputValue}
         label="MONTO TOTAL"
-        maxValue={maxCreditValue}
-        minValue={minCreditValue}
+        maxValue={maxCredit}
+        minValue={minCredit}
         onInputChange={handleCreditInputChange}
         onInputBlur={handleCreditInputBlur}
         onSliderChange={handleCreditSliderChange}
-        sliderCredit={sliderCredit}
+        sliderCredit={credit.sliderValue}
       />
       <Selector
         currency=""
-        inputValue={inputPeriod}
+        inputValue={period.inputValue}
         label="PLAZO"
         maxValue={maxPeriod}
         minValue={minPeriod}
         onInputChange={handlePeriodInputChange}
         onInputBlur={handlePeriodInputBlur}
         onSliderChange={handlePeriodSliderChange}
-        sliderCredit={sliderPeriod}
+        sliderCredit={period.sliderValue}
       />
       <div className="fee-container">
         <p className="fee-title">CUOTA FIJA POR MES</p>
-        <p className="fee-amount">$ {Math.round((credit / period) * 100) / 100}</p>
+        <p className="fee-amount">$ {Math.round((credit.value / period.value) * 100) / 100}</p>
       </div>
       <div className="buttons-container">
         <Button
